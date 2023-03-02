db95be20ca3a6d410b289a38ea5e577df45f66ec
Iaroslav Seniuk <savuk7@icloud.com>
2023-03-03 00:08:07 +0200

    btnComponent

---
+++ src/components/ButtonMain/ButtonMain.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BtnMain } from './ButtonMain.styled';
+
+export default function ButtonMain(props) {
+  return <BtnMain content={props.content}/>
+};
--- src/components/ButtonMain/ButtonMain.styled.js
+++ src/components/ButtonMain/ButtonMain.styled.js
@@ -1,1 +1,33 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+export const BtnMain = styled.button`
+  min-width: 180px;
+  height: 44px;
+
+  border: 2px solid #f59256;
+  border-radius: 40px;
+
+  &::after {
+    content: '${props => props.content}';
+    //   font-family: NEED TO FULL THIS FIELD!!!!!!!!!!!!!!!!!!!!!!;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 1.35;
+    letter-spacing: 0.04em;
+    text-align: center;
+  }
+
+  background-color: transparent;
+  color: #111111;
+  cursor: pointer;
+
+  :hover,
+  :focus {
+    background-color: #f59256;
+    color: #ffffff;
+  }
+  :active {
+    background-color: #f59256;
+    color: #ffffff;
+  }
+`;
