2f0adff4638c5881d13ee0a59c947cb68aeb1d01
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2023-03-07 13:32:46 +0200

    check for Yulia

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,16 +2,11 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://node-wizards-backend.onrender.com/api';
 export const fetchNews = async () => {
-<<<<<<< HEAD
-  const response = await axios('news', {});
-=======
   const response = await axios('news');
->>>>>>> 5e5601637964455dc29373640b78fca9a58bcf8d
   return response.data;
 };
 
 export const fetchSearchNews = async query => {
-<<<<<<< HEAD
   const response = await axios(`/news/search?title=${query}`, {});
   return response.data;
 };
@@ -19,13 +14,10 @@ export const fetchSearchNews = async query => {
 export const fetchNoticesByCategory = async query => {
   const response = await axios.get(`/notices/by-category/${query}`);
   console.log(response.data);
-=======
-  const response = await axios(`news/title?title=${query}`)
   return response.data;
 };
 
 export const fetchOurFriends = async () => {
   const response = await axios('/friends');
->>>>>>> 5e5601637964455dc29373640b78fca9a58bcf8d
   return response.data;
 };
