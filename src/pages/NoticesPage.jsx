fd25b9fc839aff4f46b8313dec5a431d64bb8f17
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2023-03-07 14:04:28 +0200

    fix

--- src/pages/NoticesPage.jsx
+++ src/pages/NoticesPage.jsx
@@ -2,10 +2,9 @@ import Categories from '../components/NoticesPage/Categories/Categories';
 import SearchForm from 'components/SearchForm/SearchForm';
 import NoticesCategoriesList from 'components/NoticesPage/NoticesCategoriesList/NoticesCategoriesList';
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { fetchNoticesByCategory } from 'services/apiService';
 export default function NoticesPage() {
-  const [notices, setNotices] = useState([]);
   const { categoryName } = useParams();
 
   console.log(categoryName);
