73d6ab8e88698059ae463a904277763980bed08a
olegtremba1234 <olegtremba@gmail.com>
2023-03-08 20:09:01 +0000

    Deploying to gh-pages from @ olegtremba1234/pets-team-node-wizards-front@c42ec4b5f468d00cf5d9b9089bc3d5fae64e98c9 🚀

---
+++ static/js/907.45545b04.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[907],{6907:function(t,e,c){c.d(e,{zob:function(){return r}});var a=c(9983);function r(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M8.2 751.4c0 8.6 3.4 17.401 10 24.001 13.2 13.2 34.8 13.2 48 0l451.8-451.8 445.2 445.2c13.2 13.2 34.8 13.2 48 0s13.2-34.8 0-48L542 251.401c-13.2-13.2-34.8-13.2-48 0l-475.8 475.8c-6.8 6.8-10 15.4-10 24.2z"}}]})(t)}}}]);
+//# sourceMappingURL=907.45545b04.chunk.js.map
