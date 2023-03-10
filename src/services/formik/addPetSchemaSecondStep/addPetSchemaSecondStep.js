7e42801c60dcfc4fcc8762df098998c0adb93210
Iaroslav Seniuk <savuk7@icloud.com>
2023-03-10 02:37:18 +0200

    add validation schemas

---
+++ src/services/formik/addPetSchemaSecondStep/addPetSchemaSecondStep.js
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+const addPetSchemaSecondStep = Yup.object({
+  comments: Yup.string('Add comment')
+    .min(8, 'Please enter a comment more than 7 character')
+    .max(120, 'Please enter a comment less than 120 character')
+    .required('Required'),
+});
+
+  export default addPetSchemaSecondStep;
