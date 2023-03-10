20e5085c8e05a68b9bf956afd82c09286f032fbf
olegtremba1234 <olegtremba@gmail.com>
2023-03-10 14:48:53 +0200

    Update ModalNotice.jsx

--- src/components/ModalNotice/ModalNotice.jsx
+++ src/components/ModalNotice/ModalNotice.jsx
@@ -75,190 +75,188 @@ export default function ModalNotice({ id, setIsModalOpen }) {
 
   const handleDelete = id => {
     deleteOwnNoticeById(id, token)
-      .then(() => toast.success('Deleted successfully'))
-      .catch(console.log);
       .then(() => {
         toast.success('Deleted successfully');
         setIsModalOpen(false);
       })
-    .catch(err => {
-      console.log(err);
-      toast.error('Oops...Something went wrong. Failed to delete');
-    });
-};
+      .catch(err => {
+        console.log(err);
+        toast.error('Oops...Something went wrong. Failed to delete');
+      });
+  };
 
-useEffect(() => {
-  fetchNoticeById(id)
-    .then(data => {
-      setNoticesDetails(data);
-      setIsFavorite(data.isFavorite);
-    })
-    .catch(console.log);
-}, [id]);
-useEffect(() => {
-  const escapeModal = event => {
-    if (event.code === 'Escape') {
-      event.preventDefault();
+  useEffect(() => {
+    fetchNoticeById(id)
+      .then(data => {
+        setNoticesDetails(data);
+        setIsFavorite(data.isFavorite);
+      })
+      .catch(console.log);
+  }, [id]);
+  useEffect(() => {
+    const escapeModal = event => {
+      if (event.code === 'Escape') {
+        event.preventDefault();
+        setIsModalOpen(false);
+      }
+    };
+    window.addEventListener('keydown', escapeModal);
+
+    return () => {
+      window.removeEventListener('keydown', escapeModal);
+    };
+  });
+  const handleCloseModal = event => {
+    if (event.currentTarget === event.target) {
       setIsModalOpen(false);
     }
   };
-  window.addEventListener('keydown', escapeModal);
 
-  return () => {
-    window.removeEventListener('keydown', escapeModal);
-  };
-});
-const handleCloseModal = event => {
-  if (event.currentTarget === event.target) {
-    setIsModalOpen(false);
-  }
-};
-
-return (
-  <>
-    {noticeDetails && (
-      <Overlay onClick={handleCloseModal}>
-        <ModalWindow>
-          <CloseBtn onClick={() => setIsModalOpen(false)}>
-            <CloseIcon />
-          </CloseBtn>
-          <ContentWrapper>
-            <div>
-              <Image
-                src={
-                  noticeDetails.petPhotoURL
-                    ? noticeDetails.petPhotoURL
-                    : defaultCat
-                }
-                alt={noticeDetails.breed}
-                loading="lazy"
-              />
-              <CategoryWrapper>
-                <CategoryText>{noticeDetails.category}</CategoryText>
-              </CategoryWrapper>
-            </div>
-            <div>
-              <H3>{noticeDetails.title} </H3>
-              <TableWrapper>
-                <Ul>
-                  <KeyLi>Name:</KeyLi>
-                  <KeyLi>Birtday:</KeyLi>
-                  <KeyLi>Breed:</KeyLi>
-                  <KeyLi>Place:</KeyLi>
-                  <KeyLi>The sex:</KeyLi>
-                  <KeyLi> Email:</KeyLi>
-                  <KeyLi>Phone:</KeyLi>
-                  {noticeDetails.category === 'sell' && <KeyLi>Price:</KeyLi>}
-                </Ul>
-                <Ul>
-                  <ValueLi>{noticeDetails.name}</ValueLi>
-                  <ValueLi>{transormDate(noticeDetails.birthday)}</ValueLi>
-                  <ValueLi>{noticeDetails.breed}</ValueLi>
-                  <ValueLi>{noticeDetails.location}</ValueLi>
-                  <ValueLi>{noticeDetails.sex}</ValueLi>
-                  <ValueLi>
-                    <Link href={`mailto:${noticeDetails.email}`}>
-                      {noticeDetails.email}
-                    </Link>
-                  </ValueLi>
-                  <ValueLi>
-                    <Link href={`tel:${noticeDetails.phone}`}>
-                      {noticeDetails.phone}
-                    </Link>
-                  </ValueLi>
-                  {noticeDetails.category === 'sell' && (
-                    <ValueLi>{noticeDetails.price} &#36;</ValueLi>
-                  )}
-                </Ul>
-              </TableWrapper>
-            </div>
-          </ContentWrapper>
-          <Comment>
-            <SpanComment>Comments: </SpanComment>
-            {noticeDetails.comments}
-          </Comment>
-          <ButtonWrapper>
-            {noticeDetails.isOwn ? (
-              <>
-                <Li>
-                  <ButtonAdd
-                    type="button"
-                    onClick={() => handleDelete(noticeDetails.id)}
-                  >
-                    Delete
-                  </ButtonAdd>
-                </Li>
-                <Li>
-                  {isFavorite ? (
-                    <ButtonRemoveFromFavorite
-                      type="button"
-                      onClick={() =>
-                        handleRemoveFromFavorite(noticeDetails.id)
-                      }
-                    >
-                      Remove from <HeartIcon />
-                    </ButtonRemoveFromFavorite>
-                  ) : (
-                    <ButtonAdd
-                      type="button"
-                      onClick={() => handleAddToFavorite(noticeDetails.id)}
-                    >
-                      Add to <HeartIcon />
-                    </ButtonAdd>
-                  )}
-                </Li>
-              </>
-            ) : (
-              <>
-                <Li>
-                  <Media
-                    queries={{
-                      small: '(max-width: 767px)',
-                      medium: '(min-width: 768px) and (max-width: 1279px)',
-                      large: '(min-width: 1280px)',
-                    }}
-                  >
-                    {matches => (
-                      <Fragment>
-                        {matches.small && (
-                          <LinkContact href={`tel:${noticeDetails.phone}`}>
-                            Contact
-                          </LinkContact>
-                        )}
-                        {(matches.medium || matches.large) && (
-                          <LinkContact href={`mailto:${noticeDetails.email}`}>
-                            Contact
-                          </LinkContact>
-                        )}
-                      </Fragment>
+  return (
+    <>
+      {noticeDetails && (
+        <Overlay onClick={handleCloseModal}>
+          <ModalWindow>
+            <CloseBtn onClick={() => setIsModalOpen(false)}>
+              <CloseIcon />
+            </CloseBtn>
+            <ContentWrapper>
+              <div>
+                <Image
+                  src={
+                    noticeDetails.petPhotoURL
+                      ? noticeDetails.petPhotoURL
+                      : defaultCat
+                  }
+                  alt={noticeDetails.breed}
+                  loading="lazy"
+                />
+                <CategoryWrapper>
+                  <CategoryText>{noticeDetails.category}</CategoryText>
+                </CategoryWrapper>
+              </div>
+              <div>
+                <H3>{noticeDetails.title} </H3>
+                <TableWrapper>
+                  <Ul>
+                    <KeyLi>Name:</KeyLi>
+                    <KeyLi>Birtday:</KeyLi>
+                    <KeyLi>Breed:</KeyLi>
+                    <KeyLi>Place:</KeyLi>
+                    <KeyLi>The sex:</KeyLi>
+                    <KeyLi> Email:</KeyLi>
+                    <KeyLi>Phone:</KeyLi>
+                    {noticeDetails.category === 'sell' && <KeyLi>Price:</KeyLi>}
+                  </Ul>
+                  <Ul>
+                    <ValueLi>{noticeDetails.name}</ValueLi>
+                    <ValueLi>{transormDate(noticeDetails.birthday)}</ValueLi>
+                    <ValueLi>{noticeDetails.breed}</ValueLi>
+                    <ValueLi>{noticeDetails.location}</ValueLi>
+                    <ValueLi>{noticeDetails.sex}</ValueLi>
+                    <ValueLi>
+                      <Link href={`mailto:${noticeDetails.email}`}>
+                        {noticeDetails.email}
+                      </Link>
+                    </ValueLi>
+                    <ValueLi>
+                      <Link href={`tel:${noticeDetails.phone}`}>
+                        {noticeDetails.phone}
+                      </Link>
+                    </ValueLi>
+                    {noticeDetails.category === 'sell' && (
+                      <ValueLi>{noticeDetails.price} &#36;</ValueLi>
                     )}
-                  </Media>
-                </Li>
-                <Li>
-                  {isFavorite ? (
-                    <ButtonRemoveFromFavorite
-                      type="button"
-                      onClick={() =>
-                        handleRemoveFromFavorite(noticeDetails.id)
-                      }
-                    >
-                      Remove from <HeartIcon />
-                    </ButtonRemoveFromFavorite>
-                  ) : (
+                  </Ul>
+                </TableWrapper>
+              </div>
+            </ContentWrapper>
+            <Comment>
+              <SpanComment>Comments: </SpanComment>
+              {noticeDetails.comments}
+            </Comment>
+            <ButtonWrapper>
+              {noticeDetails.isOwn ? (
+                <>
+                  <Li>
                     <ButtonAdd
                       type="button"
-                      onClick={() => handleAddToFavorite(noticeDetails.id)}
+                      onClick={() => handleDelete(noticeDetails.id)}
                     >
-                      Add to <HeartIcon />
+                      Delete
                     </ButtonAdd>
-                  )}
-                </Li>
-              </>
-            )}
-          </ButtonWrapper>
-        </ModalWindow>
-      </Overlay>
-    )}
-  </>
-);
+                  </Li>
+                  <Li>
+                    {isFavorite ? (
+                      <ButtonRemoveFromFavorite
+                        type="button"
+                        onClick={() =>
+                          handleRemoveFromFavorite(noticeDetails.id)
+                        }
+                      >
+                        Remove from <HeartIcon />
+                      </ButtonRemoveFromFavorite>
+                    ) : (
+                      <ButtonAdd
+                        type="button"
+                        onClick={() => handleAddToFavorite(noticeDetails.id)}
+                      >
+                        Add to <HeartIcon />
+                      </ButtonAdd>
+                    )}
+                  </Li>
+                </>
+              ) : (
+                <>
+                  <Li>
+                    <Media
+                      queries={{
+                        small: '(max-width: 767px)',
+                        medium: '(min-width: 768px) and (max-width: 1279px)',
+                        large: '(min-width: 1280px)',
+                      }}
+                    >
+                      {matches => (
+                        <Fragment>
+                          {matches.small && (
+                            <LinkContact href={`tel:${noticeDetails.phone}`}>
+                              Contact
+                            </LinkContact>
+                          )}
+                          {(matches.medium || matches.large) && (
+                            <LinkContact href={`mailto:${noticeDetails.email}`}>
+                              Contact
+                            </LinkContact>
+                          )}
+                        </Fragment>
+                      )}
+                    </Media>
+                  </Li>
+                  <Li>
+                    {isFavorite ? (
+                      <ButtonRemoveFromFavorite
+                        type="button"
+                        onClick={() =>
+                          handleRemoveFromFavorite(noticeDetails.id)
+                        }
+                      >
+                        Remove from <HeartIcon />
+                      </ButtonRemoveFromFavorite>
+                    ) : (
+                      <ButtonAdd
+                        type="button"
+                        onClick={() => handleAddToFavorite(noticeDetails.id)}
+                      >
+                        Add to <HeartIcon />
+                      </ButtonAdd>
+                    )}
+                  </Li>
+                </>
+              )}
+            </ButtonWrapper>
+          </ModalWindow>
+        </Overlay>
+      )}
+    </>
+  );
 }
