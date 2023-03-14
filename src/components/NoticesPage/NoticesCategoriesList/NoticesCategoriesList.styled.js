7651052eed44f91240abf3295885b07405cd10e2
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-03-14 16:03:10 +0200

    add dollar sign

--- src/components/NoticesPage/NoticesCategoriesList/NoticesCategoriesList.styled.js
+++ src/components/NoticesPage/NoticesCategoriesList/NoticesCategoriesList.styled.js
@@ -3,5 +3,9 @@ import styled from 'styled-components';
 export const NoticeList = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   gap: 32px;
+  @media (min-width: 768px) {
+    justify-content: flex-start;
+  }
 `;
